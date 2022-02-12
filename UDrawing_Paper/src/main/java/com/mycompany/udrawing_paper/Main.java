589c76669bb95f814e6e8f0db5219b93519597b8
JJONK19 <josuejaviera19@gmail.com>
2022-02-12 00:51:33 -0600

    Menú

--- UDrawing_Paper/src/main/java/com/mycompany/udrawing_paper/Main.java
+++ UDrawing_Paper/src/main/java/com/mycompany/udrawing_paper/Main.java
@@ -4,15 +4,115 @@
  */
 package com.mycompany.udrawing_paper;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author JJONK19
  */
 public class Main {
+    static int carga = 0; //Bandera que indica si ya se cargó la información
+    static int ventanas = 0; //Guarda el número de ventanillas. Si el usuario asigna 0 o no lo hace, muestra error.
+    Cola Clientes = new Cola(); //Cola de Clientes que entran a la empresa
+    ListaPilas Ventanilla = new ListaPilas(); //Ventanillas de la empresa
+    ListaSimple Atendidos = new ListaSimple();
+    Cola BN = new Cola(); //Impresora en blanco y negro
+    Cola C = new Cola(); //Impresora a color
+    ListaDCircularL Espera = new ListaDCircularL(); //Lista de Clientes esperando
+    
     
     
     public static void main(String[] args){
-        Main.prueba();
+        String x = "";
+        Scanner a = new Scanner(System.in);
+        while(!"0".equals(x)){
+            System.out.println("UDrawing Paper");
+            System.out.println("MENÚ PRINCIPAL");
+            System.out.println("1. Paraametros Iniciales");
+            System.out.println("2. Ejecutar Paso");
+            System.out.println("3. Estado en memoría de las estructuras");
+            System.out.println("4. Reportes");
+            System.out.println("5. Acerca de");
+            System.out.println("0. Salir");
+            System.out.println("---------------------------------------------------------");
+            System.out.println("Ingrese el número de una instrucción:");
+            x = String.valueOf(a.nextLine());  
+            System.out.println("---------------------------------------------------------");
+            
+            //Carga y Definición de Datos Iniciales***********************************************
+            if ("1".equals(x)){
+                String x1 = "";
+                while(!"0".equals(x1)){
+                    System.out.println("UDrawing Paper");
+                    System.out.println("PARAMETROS INICIALES");
+                    System.out.println("1. Carga de Clientes");
+                    System.out.println("2. Definir número de ventanillas");
+                    System.out.println("0. Salir");
+                    System.out.println("---------------------------------------------------------");
+                    System.out.println("Ingrese el número de una instrucción:");
+                    x1 = String.valueOf(a.nextLine());  
+                    System.out.println("---------------------------------------------------------");
+                    
+                    //Carga de datos
+                    if ("1".equals(x1)){
+                        try{
+                            Scanner a1 = new Scanner(System.in);
+                            String ruta;
+                            System.out.println("Ingrese la ruta del archivo:");
+                            ruta = a1.nextLine();  
+                            File file = new File(ruta);
+                            Scanner input = new Scanner(file);
+                            String txt = "";
+                            while(input.hasNextLine()){
+                                txt += input.nextLine();
+                            }
+                            
+                            
+                        }catch(Exception e){
+                            System.out.println("");
+                            System.out.println("ERROR: No se pudo leer el archivo de carga.");
+                            System.out.println("");
+                            System.out.println("---------------------------------------------------------");
+                        }
+                    }
+
+                    //Número de Ventanillas
+                    if ("2".equals(x1)){
+                        System.out.println("Ingrese el número de ventanillas a manejar:");
+                        ventanas = a.nextInt();  
+                        System.out.println("---------------------------------------------------------");
+                        System.out.println("");
+                        System.out.println("MENSAJE: Número de ventanillas actualizado.");
+                        System.out.println("");
+                        System.out.println("---------------------------------------------------------");
+                    }  
+                } 
+            }
+            
+            if ("2".equals(x)){
+                System.out.println("Funciona");
+            }
+            
+            if ("3".equals(x)){
+                System.out.println("Funciona");
+            }
+            
+            if ("4".equals(x)){
+                System.out.println("Funciona");
+            }
+            
+            if ("5".equals(x)){
+                System.out.println("ESTRUCTURAS DE DATOS SECCIÓN C");
+                System.out.println("Josué Javier Aguilar López");
+                System.out.println("201909035");
+                System.out.println("---------------------------------------------------------");
+            }
+           
+            
+        }
     }
     
     //Metodo de ejemplo para usar estructuras
