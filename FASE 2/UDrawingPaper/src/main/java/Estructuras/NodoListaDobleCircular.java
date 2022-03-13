5e9ef0a717e007f8c507cccf338debb2d79bb7c7
JJONK19 <josuejaviera19@gmail.com>
2022-03-13 13:50:52 -0600

    Creación del proyecto
    -Mover carpetas
    -Extraer estructuras de la fase 1

---
+++ FASE 2/UDrawingPaper/src/main/java/Estructuras/NodoListaDobleCircular.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructuras;
+import java.util.UUID;
+
+/**
+ *
+ * @author JJONK19
+ */
+public class NodoListaDobleCircular {
+    String ID;          //ID del nodo. Tiene uso en el gráfico.
+    Object content;     //Contenido del Nodo
+    NodoListaDobleCircular next;      //Apuntador al siguiente nodo de la lista
+    NodoListaDobleCircular prev;      //Apuntador al anterior nodo de la lista
+    String name;        //Nombre que va a aparecer en el grafo
+    Object structure;   //Contiene una estructura auxiliar en caso de ser una lista de-
+    
+    public NodoListaDobleCircular(Object _structure, String _name, Object _content){
+        this.ID = "\""+UUID.randomUUID().toString() + "\"";
+        this.structure = _structure;
+        this.next = null;
+        this.prev = null;
+        this.name = _name;
+        this.content = _content;
+        
+    }
+    
+    //Borra la estructura asociada al nodo 
+    public void deleteS(){
+        this.structure = null;
+    }
+    
+    //Elimina el contenido del nodo
+    public void deleteC(){
+        this.content = null;
+    }
+    
+        
+}
